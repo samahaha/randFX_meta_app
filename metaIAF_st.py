701c267d7336cd37efcfa7dd7c79fd86be02350e
Jason Samaha <jasonsamaha@gmail.com>
2023-08-09 01:14:21 -0700

    Update metaIAF_st.py

--- metaIAF_st.py
+++ metaIAF_st.py
@@ -62,8 +62,8 @@ with col1:
 er = np.array(edited_df.r,dtype=float)
 en = np.array(edited_df.n,dtype=float)
 
-er = er[(~np.isnan(er)) & ~np.isnan(en)]
-en = en[(~np.isnan(er)) & ~np.isnan(en)]
+er = er[(~np.isnan(er)) & (~np.isnan(en))]
+en = en[(~np.isnan(er)) & (~np.isnan(en))]
 
 
 meta_r = sum(er*en)/sum(en)
